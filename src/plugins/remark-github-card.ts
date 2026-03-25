8fc359f35314b00cb8403323e469a36bb76e7959
Chris McC <mcclelland.c@gmail.com>
2026-03-25 15:13:02 +0000

    replace biome with oxlint/oxfmt.

--- src/plugins/remark-github-card.ts
+++ src/plugins/remark-github-card.ts
@@ -1,35 +1,36 @@
 import type { Root } from "mdast";
 import type { Plugin } from "unified";
 import { visit } from "unist-util-visit";
+
 import { h, isNodeDirective } from "../utils/remark";
 
 const DIRECTIVE_NAME = "github";
 
 export const remarkGithubCard: Plugin<[], Root> = () => (tree) => {
-	visit(tree, (node, index, parent) => {
-		if (!parent || index === undefined || !isNodeDirective(node)) return;
+  visit(tree, (node, index, parent) => {
+    if (!parent || index === undefined || !isNodeDirective(node)) return;
 
-		// We only want a leaf directive named DIRECTIVE_NAME
-		if (node.type !== "leafDirective" || node.name !== DIRECTIVE_NAME) return;
+    // We only want a leaf directive named DIRECTIVE_NAME
+    if (node.type !== "leafDirective" || node.name !== DIRECTIVE_NAME) return;
 
-		let repoName = node.attributes?.repo ?? node.attributes?.user ?? null;
-		if (!repoName) return; // Leave the directive as-is if no repo is provided
+    let repoName = node.attributes?.repo ?? node.attributes?.user ?? null;
+    if (!repoName) return; // Leave the directive as-is if no repo is provided
 
-		repoName = repoName.endsWith("/") ? repoName.slice(0, -1) : repoName; // Remove trailing slash
-		repoName = repoName.startsWith("https://github.com/")
-			? repoName.replace("https://github.com/", "")
-			: repoName; // Remove leading URL
+    repoName = repoName.endsWith("/") ? repoName.slice(0, -1) : repoName; // Remove trailing slash
+    repoName = repoName.startsWith("https://github.com/")
+      ? repoName.replace("https://github.com/", "")
+      : repoName; // Remove leading URL
 
-		const repoParts = repoName.split("/");
-		const SimpleUUID = `GC-${crypto.randomUUID()}`;
-		const realUrl = `https://github.com/${repoName}`;
+    const repoParts = repoName.split("/");
+    const SimpleUUID = `GC-${crypto.randomUUID()}`;
+    const realUrl = `https://github.com/${repoName}`;
 
-		// If its a repo link
-		if (repoParts.length > 1) {
-			const script = h("script", {}, [
-				{
-					type: "text",
-					value: `
+    // If its a repo link
+    if (repoParts.length > 1) {
+      const script = h("script", {}, [
+        {
+          type: "text",
+          value: `
 				fetch('https://api.github.com/repos/${repoName}', { referrerPolicy: "no-referrer" })
 					.then(response => response.json())
 					.then(data => {
@@ -58,49 +59,49 @@ export const remarkGithubCard: Plugin<[], Root> = () => (tree) => {
          	console.warn("[GITHUB-CARD] Error loading card for ${repoName} | ${SimpleUUID}.", err)
       	})
 			`,
-				},
-			]);
+        },
+      ]);
 
-			const hTitle = h("div", { class: "gh-title title" }, [
-				h("span", { class: "gh-avatar" }),
-				h("a", { class: "gh-text not-prose cactus-link", href: realUrl }, [
-					{ type: "text", value: `${repoParts[0]}/${repoParts[1]}` },
-				]),
-				h("span", { class: "gh-icon" }),
-			]);
+      const hTitle = h("div", { class: "gh-title title" }, [
+        h("span", { class: "gh-avatar" }),
+        h("a", { class: "gh-text not-prose cactus-link", href: realUrl }, [
+          { type: "text", value: `${repoParts[0]}/${repoParts[1]}` },
+        ]),
+        h("span", { class: "gh-icon" }),
+      ]);
 
-			const hChips = h("div", { class: "gh-chips" }, [
-				h("span", { class: "gh-stars" }, [{ type: "text", value: "00K" }]),
-				h("span", { class: "gh-forks" }, [{ type: "text", value: "00K" }]),
-				h("span", { class: "gh-license" }, [{ type: "text", value: "MIT" }]),
-				h("span", { class: "gh-language" }, [{ type: "text", value: "" }]),
-			]);
+      const hChips = h("div", { class: "gh-chips" }, [
+        h("span", { class: "gh-stars" }, [{ type: "text", value: "00K" }]),
+        h("span", { class: "gh-forks" }, [{ type: "text", value: "00K" }]),
+        h("span", { class: "gh-license" }, [{ type: "text", value: "MIT" }]),
+        h("span", { class: "gh-language" }, [{ type: "text", value: "" }]),
+      ]);
 
-			const hDescription = h("div", { class: "gh-description" }, [
-				{
-					type: "text",
-					value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-				},
-			]);
+      const hDescription = h("div", { class: "gh-description" }, [
+        {
+          type: "text",
+          value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        },
+      ]);
 
-			parent.children.splice(
-				index,
-				1,
-				h("div", { id: SimpleUUID, class: "github-card gh-loading" }, [
-					hTitle,
-					hDescription,
-					hChips,
-					script,
-				]),
-			);
-		}
+      parent.children.splice(
+        index,
+        1,
+        h("div", { id: SimpleUUID, class: "github-card gh-loading" }, [
+          hTitle,
+          hDescription,
+          hChips,
+          script,
+        ]),
+      );
+    }
 
-		// If its a user link
-		else if (repoParts.length === 1) {
-			const script = h("script", {}, [
-				{
-					type: "text",
-					value: `
+    // If its a user link
+    else if (repoParts.length === 1) {
+      const script = h("script", {}, [
+        {
+          type: "text",
+          value: `
 				fetch('https://api.github.com/users/${repoName}', { referrerPolicy: "no-referrer" })
 					.then(response => response.json())
 					.then(data => {
@@ -119,28 +120,28 @@ export const remarkGithubCard: Plugin<[], Root> = () => (tree) => {
          	console.warn("[GITHUB-CARD] Error loading card for ${repoName} | ${SimpleUUID}.", err)
       	})
 			`,
-				},
-			]);
+        },
+      ]);
 
-			parent.children.splice(
-				index,
-				1,
-				h("div", { id: SimpleUUID, class: "github-card gh-simple gh-loading" }, [
-					h("div", { class: "gh-title title" }, [
-						h("span", { class: "gh-avatar" }),
-						h("a", { class: "gh-text not-prose cactus-link", href: realUrl }, [
-							{ type: "text", value: repoParts[0] },
-						]),
-						h("span", { class: "gh-icon" }),
-					]),
-					h("div", { class: "gh-chips" }, [
-						h("span", { class: "gh-followers" }, [{ type: "text", value: "00K" }]),
-						h("span", { class: "gh-repositories" }, [{ type: "text", value: "00K" }]),
-						h("span", { class: "gh-region" }, [{ type: "text", value: "" }]),
-					]),
-					script,
-				]),
-			);
-		}
-	});
+      parent.children.splice(
+        index,
+        1,
+        h("div", { id: SimpleUUID, class: "github-card gh-simple gh-loading" }, [
+          h("div", { class: "gh-title title" }, [
+            h("span", { class: "gh-avatar" }),
+            h("a", { class: "gh-text not-prose cactus-link", href: realUrl }, [
+              { type: "text", value: repoParts[0] },
+            ]),
+            h("span", { class: "gh-icon" }),
+          ]),
+          h("div", { class: "gh-chips" }, [
+            h("span", { class: "gh-followers" }, [{ type: "text", value: "00K" }]),
+            h("span", { class: "gh-repositories" }, [{ type: "text", value: "00K" }]),
+            h("span", { class: "gh-region" }, [{ type: "text", value: "" }]),
+          ]),
+          script,
+        ]),
+      );
+    }
+  });
 };
--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -4,20 +4,20 @@ import type { Directives } from "mdast-util-directive";
 
 /** Checks if a node is a directive. */
 export function isNodeDirective(node: Node): node is Directives {
-	return (
-		node.type === "containerDirective" ||
-		node.type === "leafDirective" ||
-		node.type === "textDirective"
-	);
+  return (
+    node.type === "containerDirective" ||
+    node.type === "leafDirective" ||
+    node.type === "textDirective"
+  );
 }
 
 /** From Astro Starlight: Function that generates an mdast HTML tree ready for conversion to HTML by rehype. */
 // biome-ignore lint/suspicious/noExplicitAny: allow any children
 export function h(el: string, attrs: Properties = {}, children: any[] = []): P {
-	const { properties, tagName } = _h(el, attrs);
-	return {
-		children,
-		data: { hName: tagName, hProperties: properties },
-		type: "paragraph",
-	};
+  const { properties, tagName } = _h(el, attrs);
+  return {
+    children,
+    data: { hName: tagName, hProperties: properties },
+    type: "paragraph",
+  };
 }
